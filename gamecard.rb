f9b9fe17811ce7b93b9e2046cc082350a4a36883
hoamorix <93793891+hoamorix@users.noreply.github.com>
2021-11-17 23:43:35 +0300

    Update gamecard.rb

--- gamecard.rb
+++ gamecard.rb
@@ -5,34 +5,34 @@ username = gets.chomp
 
 puts "Привет #{username}, добро пожаловать в игру!"
 
-users_desk = DESK_OF_CARDS.to_a.shuffle.to_h
-bots_desk = DESK_OF_CARDS.to_a.shuffle.to_h
+user_desk = DESK_OF_CARDS.to_a.shuffle.to_h
+bot_desk = DESK_OF_CARDS.to_a.shuffle.to_h
 user_score = 0
 bot_score = 0
 
 loop do
-break if users_desk.size == 0
+break if user_desk.size == 0
 
-puts "Введите любое число от 0 до #{users_desk.size - 1}"if users_desk.size > 1
+puts "Введите любое число от 0 до #{user_desk.size - 1}"if user_desk.size > 1
 
-users_input = users_desk.size > 1 ? gets.to_i : 0
-users_step = users_desk.to_a.delete_at(users_input)
-bots_step = bots_desk.to_a.delete_at(rand(0...(bots_desk.size)))
+user_input = user_desk.size > 1 ? gets.to_i : 0
+user_step = user_desk.to_a.delete_at(user_input)
+bot_step = bot_desk.to_a.delete_at(rand(0...(bot_desk.size)))
 
-users_desk.delete(users_step[0])
-bots_desk.delete(bots_step[0])
+user_desk.delete(user_step[0])
+bot_desk.delete(bot_step[0])
 
-puts 'Последний ход!' if users_desk.size == 0
+puts 'Последний ход!' if user_desk.size == 0
 sleep (rand(1..2))
-puts "Ваша карта #{users_step[0]} победила. У Компьютера #{bots_step[0]}" if users_step[1] > bots_step[1]
+puts "Ваша карта #{user_step[0]} победила. У Компьютера #{bot_step[0]}" if user_step[1] > bot_step[1]
 
-user_score += 1 if users_step[1] > bots_step[1]
+user_score += 1 if user_step[1] > bot_step[1]
 
-puts "Ваша карта #{users_step[0]} проиграла. У Компьютера #{bots_step[0]}" if users_step[1] < bots_step[1]
+puts "Ваша карта #{user_step[0]} проиграла. У Компьютера #{bot_step[0]}" if user_step[1] < bot_step[1]
 
-bot_score += 1 if users_step[1] < bots_step[1]
+bot_score += 1 if user_step[1] < bot_step[1]
 
-puts "Ого вот это удача #{users_step[0]}" if users_step[1] == bots_step[1]
+puts "Ого вот это удача #{user_step[0]}" if user_step[1] == bot_step[1]
 sleep 1
-puts user_score > bot_score ? "#{username} Вы победили" : "Компьютер проиграл" if users_desk.size == 0
+puts user_score > bot_score ? "#{username} Вы победили" : "Компьютер проиграл" if user_desk.size == 0
 end
